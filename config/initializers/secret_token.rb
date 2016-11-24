4dea39e84d0bc2767d512ec0b88982bb4fa0bd1c
Marcos Teixeira <m.viniteixeira@gmail.com>
2016-11-24 08:37:39 -0200

    Starting rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CurrencyConversion::Application.config.secret_token = 'ffe089cbe308af8615f5d17962acb8aa05b42c162eec58d7ae868ed20b10710b292e36450f33b46ec8c3e89e6058d9faa903d96ec2471a7bb89b383c3530a8e3'
